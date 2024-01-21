49e3176899aceaec43db017352e555dccb9dbcd3
Restyled.io <commits@restyled.io>
2024-01-21 09:47:35 +0000

    Restyled by clang-format

--- include/astra_camera/astra_device.h
+++ include/astra_camera/astra_device.h
@@ -37,135 +37,137 @@
 
 #include "astra_camera/astra_exception.h"
 
-#include <boost/shared_ptr.hpp>
-#include <boost/cstdint.hpp>
 #include <boost/bind.hpp>
+#include <boost/cstdint.hpp>
 #include <boost/function.hpp>
+#include <boost/shared_ptr.hpp>
 
 #include <sensor_msgs/msg/image.hpp>
 
 #include <string>
 #include <vector>
 
-namespace openni
-{
+namespace openni {
 class Device;
 class DeviceInfo;
 class VideoStream;
 class SensorInfo;
-}
+} // namespace openni
 
-namespace astra_wrapper
-{
+namespace astra_wrapper {
 
-typedef boost::function<void(sensor_msgs::msg::Image::SharedPtr image)> FrameCallbackFunction;
+typedef boost::function<void(sensor_msgs::msg::Image::SharedPtr image)>
+    FrameCallbackFunction;
 
 class AstraFrameListener;
 
-class AstraDevice
-{
+class AstraDevice {
 public:
-    AstraDevice(const std::string& device_URI) throw (AstraException);
-    virtual ~AstraDevice();
+  AstraDevice(const std::string &device_URI) throw(AstraException);
+  virtual ~AstraDevice();
 
-    const std::string getUri() const;
-    const std::string getVendor() const;
-    const std::string getName() const;
-    uint16_t getUsbVendorId() const;
-    uint16_t getUsbProductId() const;
+  const std::string getUri() const;
+  const std::string getVendor() const;
+  const std::string getName() const;
+  uint16_t getUsbVendorId() const;
+  uint16_t getUsbProductId() const;
 
-    const std::string getStringID() const;
+  const std::string getStringID() const;
 
-    bool isValid() const;
+  bool isValid() const;
 
-    bool hasIRSensor() const;
-    bool hasColorSensor() const;
-    bool hasDepthSensor() const;
+  bool hasIRSensor() const;
+  bool hasColorSensor() const;
+  bool hasDepthSensor() const;
 
-    void startIRStream();
-    void startColorStream();
-    void startDepthStream();
+  void startIRStream();
+  void startColorStream();
+  void startDepthStream();
 
-    void stopAllStreams();
+  void stopAllStreams();
 
-    void stopIRStream();
-    void stopColorStream();
-    void stopDepthStream();
+  void stopIRStream();
+  void stopColorStream();
+  void stopDepthStream();
 
-    bool isIRStreamStarted();
-    bool isColorStreamStarted();
-    bool isDepthStreamStarted();
+  bool isIRStreamStarted();
+  bool isColorStreamStarted();
+  bool isDepthStreamStarted();
 
-    bool isImageRegistrationModeSupported() const;
-    void setImageRegistrationMode(bool enabled) throw (AstraException);
-    void setDepthColorSync(bool enabled) throw (AstraException);
+  bool isImageRegistrationModeSupported() const;
+  void setImageRegistrationMode(bool enabled) throw(AstraException);
+  void setDepthColorSync(bool enabled) throw(AstraException);
 
-    const AstraVideoMode getIRVideoMode() throw (AstraException);
-    const AstraVideoMode getColorVideoMode() throw (AstraException);
-    const AstraVideoMode getDepthVideoMode() throw (AstraException);
+  const AstraVideoMode getIRVideoMode() throw(AstraException);
+  const AstraVideoMode getColorVideoMode() throw(AstraException);
+  const AstraVideoMode getDepthVideoMode() throw(AstraException);
 
-    const std::vector<AstraVideoMode>& getSupportedIRVideoModes() const;
-    const std::vector<AstraVideoMode>& getSupportedColorVideoModes() const;
-    const std::vector<AstraVideoMode>& getSupportedDepthVideoModes() const;
+  const std::vector<AstraVideoMode> &getSupportedIRVideoModes() const;
+  const std::vector<AstraVideoMode> &getSupportedColorVideoModes() const;
+  const std::vector<AstraVideoMode> &getSupportedDepthVideoModes() const;
 
-    bool isIRVideoModeSupported(const AstraVideoMode& video_mode) const;
-    bool isColorVideoModeSupported(const AstraVideoMode& video_mode) const;
-    bool isDepthVideoModeSupported(const AstraVideoMode& video_mode) const;
+  bool isIRVideoModeSupported(const AstraVideoMode &video_mode) const;
+  bool isColorVideoModeSupported(const AstraVideoMode &video_mode) const;
+  bool isDepthVideoModeSupported(const AstraVideoMode &video_mode) const;
 
-    void setIRVideoMode(const AstraVideoMode& video_mode) throw (AstraException);
-    void setColorVideoMode(const AstraVideoMode& video_mode) throw (AstraException);
-    void setDepthVideoMode(const AstraVideoMode& video_mode) throw (AstraException);
+  void setIRVideoMode(const AstraVideoMode &video_mode) throw(AstraException);
+  void
+  setColorVideoMode(const AstraVideoMode &video_mode) throw(AstraException);
+  void
+  setDepthVideoMode(const AstraVideoMode &video_mode) throw(AstraException);
 
-    void setIRFrameCallback(FrameCallbackFunction callback);
-    void setColorFrameCallback(FrameCallbackFunction callback);
-    void setDepthFrameCallback(FrameCallbackFunction callback);
+  void setIRFrameCallback(FrameCallbackFunction callback);
+  void setColorFrameCallback(FrameCallbackFunction callback);
+  void setDepthFrameCallback(FrameCallbackFunction callback);
 
-    float getIRFocalLength (int output_y_resolution) const;
-    float getColorFocalLength (int output_y_resolution) const;
-    float getDepthFocalLength (int output_y_resolution) const;
+  float getIRFocalLength(int output_y_resolution) const;
+  float getColorFocalLength(int output_y_resolution) const;
+  float getDepthFocalLength(int output_y_resolution) const;
 
-    void setAutoExposure(bool enable) throw (AstraException);
-    void setAutoWhiteBalance(bool enable) throw (AstraException);
+  void setAutoExposure(bool enable) throw(AstraException);
+  void setAutoWhiteBalance(bool enable) throw(AstraException);
 
-    bool getAutoExposure() const;
-    bool getAutoWhiteBalance() const;
+  bool getAutoExposure() const;
+  bool getAutoWhiteBalance() const;
 
-    void setUseDeviceTimer(bool enable);
+  void setUseDeviceTimer(bool enable);
 
 protected:
-    void shutdown();
+  void shutdown();
 
-    boost::shared_ptr<openni::VideoStream> getIRVideoStream() const throw (AstraException);
-    boost::shared_ptr<openni::VideoStream> getColorVideoStream() const throw (AstraException);
-    boost::shared_ptr<openni::VideoStream> getDepthVideoStream() const throw (AstraException);
+  boost::shared_ptr<openni::VideoStream> getIRVideoStream() const
+      throw(AstraException);
+  boost::shared_ptr<openni::VideoStream> getColorVideoStream() const
+      throw(AstraException);
+  boost::shared_ptr<openni::VideoStream> getDepthVideoStream() const
+      throw(AstraException);
 
-    boost::shared_ptr<openni::Device> openni_device_;
-    boost::shared_ptr<openni::DeviceInfo> device_info_;
+  boost::shared_ptr<openni::Device> openni_device_;
+  boost::shared_ptr<openni::DeviceInfo> device_info_;
 
-    boost::shared_ptr<AstraFrameListener> ir_frame_listener;
-    boost::shared_ptr<AstraFrameListener> color_frame_listener;
-    boost::shared_ptr<AstraFrameListener> depth_frame_listener;
+  boost::shared_ptr<AstraFrameListener> ir_frame_listener;
+  boost::shared_ptr<AstraFrameListener> color_frame_listener;
+  boost::shared_ptr<AstraFrameListener> depth_frame_listener;
 
-    mutable boost::shared_ptr<openni::VideoStream> ir_video_stream_;
-    mutable boost::shared_ptr<openni::VideoStream> color_video_stream_;
-    mutable boost::shared_ptr<openni::VideoStream> depth_video_stream_;
+  mutable boost::shared_ptr<openni::VideoStream> ir_video_stream_;
+  mutable boost::shared_ptr<openni::VideoStream> color_video_stream_;
+  mutable boost::shared_ptr<openni::VideoStream> depth_video_stream_;
 
-    mutable std::vector<AstraVideoMode> ir_video_modes_;
-    mutable std::vector<AstraVideoMode> color_video_modes_;
-    mutable std::vector<AstraVideoMode> depth_video_modes_;
+  mutable std::vector<AstraVideoMode> ir_video_modes_;
+  mutable std::vector<AstraVideoMode> color_video_modes_;
+  mutable std::vector<AstraVideoMode> depth_video_modes_;
 
-    bool ir_video_started_;
-    bool color_video_started_;
-    bool depth_video_started_;
+  bool ir_video_started_;
+  bool color_video_started_;
+  bool depth_video_started_;
 
-    bool image_registration_activated_;
+  bool image_registration_activated_;
 
-    bool use_device_time_;
-
+  bool use_device_time_;
 };
 
-std::ostream& operator << (std::ostream& stream, const AstraDevice& device);
+std::ostream &operator<<(std::ostream &stream, const AstraDevice &device);
 
-}
+} // namespace astra_wrapper
 
 #endif /* OPENNI_DEVICE_H */
